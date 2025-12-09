f48aaaaf2cf7fc00de5ca5b206d32841b11dc7fa
Frederic.Schneider <freddy25082k@gmail.com>
2025-12-09 19:08:31 +0100

    Start API-call to n8n
    Expanded the subjectTest model, added requests to requirements, added prototyp for post request to n8n

--- backend/models/subject_tests.py
+++ backend/models/subject_tests.py
@@ -1,21 +1,30 @@
 from datetime import datetime, timezone
-from typing import Optional
-from sqlmodel import Column, DateTime, SQLModel, Field
+from typing import Optional, Annotated
+from pydantic import StringConstraints
+from sqlmodel import SQLModel, Field
 
 class SubjectTestBase(SQLModel):
-    test: Optional[str] = None  # XML-Inhalt als TEXT
-    question_type: Optional[str] = None
+    name: Annotated[str, StringConstraints(strip_whitespace=True, min_length=1)] = Field(nullable=False)
 
 class SubjectTest(SubjectTestBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    # Gibt es eine Möglichkeit das direkt als XML abzuspeichern?
+    test: Optional[str] = None  # XML-Inhalt als TEXT
+    question_type: Optional[str] = None
     subject_id: int = Field(foreign_key="subject.id", nullable=False)
-    
-    created_at: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), nullable=False),
-        default_factory=lambda: datetime.now(timezone.utc),
-    )
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
 
-    updated_at: datetime = Field(
-        sa_column=Column(DateTime(timezone=True), nullable=False),
-        default_factory=lambda: datetime.now(timezone.utc),
-    )
+class SubjectTestCreate(SubjectTestBase):
+    # Potenziell mehr Daten, je nachdem was für Erstellung in n8n gebraucht wird
+    subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
+    question_type: Optional[str] = Field(nullable=False)
+
+class SubjectTestRead(SubjectTestBase):
+    id: int
+    # Gibt es eine Möglichkeit das direkt als XML abzuspeichern?
+    test: str
+    question_type: str
+    subject_id: int
+    created_at: datetime
+    updated_at: datetime
--- backend/routers/subject_tests.py
+++ backend/routers/subject_tests.py
@@ -1,6 +1,40 @@
-from fastapi import APIRouter
-from models.subject_tests import SubjectTest
+from fastapi import APIRouter, HTTPException
+from models.subject_tests import SubjectTest, SubjectTestCreate, SubjectTestRead
+from database import SessionDep
+import requests
+from config.logger_config import logger
 
 router = APIRouter(prefix="/subjecttests", tags=["subjecttests"])
 
-# Endpoints will be added later
+@router.post("/", response_model=SubjectTestRead, status_code=201)
+def create_subjectTest(
+    subjectTest_create: SubjectTestCreate,
+    session: SessionDep
+):
+    # Test von n8n anfordern
+    webhook_url = "https://n8n.Forschungsding.com/start"
+    header = {"Content-Type": "application/json; charset=utf-8"}
+    data = {
+        "question_type": subjectTest_create.question_type
+    }
+    response = requests.post(webhook_url, headers=header, json=data)
+
+    if not response.status_code == 200:
+        logger.warning(f"n8n request failed")
+        raise HTTPException(status_code=response.status_code, detail="Failed to generate test") 
+
+    # Test einspeisen
+    test = response.json()
+    return test
+
+    
+    # db_subject = SubjectTest(
+    #     name=subject_create.name,
+    #     user_id=subject_create.user_id
+    # )
+
+    # session.add(db_subject)
+    # session.commit()
+    # session.refresh(db_subject)
+    # logger.info(f"Subject {db_subject.name} (ID: {db_subject.id}) for user-id {db_subject.user_id} has been created")
+    # return db_subject
