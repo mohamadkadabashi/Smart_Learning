79d098bd467b709770e6f0b00c846106462f2974
Frederic.Schneider <freddy25082k@gmail.com>
2025-12-09 19:08:31 +0100

    Start API-call to n8n
    Expanded the subjectTest model, added requests to requirements, added prototyp for post request to n8n

--- backend/models/subject_tests.py
+++ backend/models/subject_tests.py
@@ -9,31 +9,23 @@ class SubjectTestBase(SQLModel):
 
 class SubjectTest(SubjectTestBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-     # XML as TEXT
-    test: Optional[str] = None
-    question_type: str = None
-    question_count: int = Field(default=None)
+    # Gibt es eine Möglichkeit das direkt als XML abzuspeichern?
+    test: Optional[str] = None  # XML-Inhalt als TEXT
+    question_type: Optional[str] = None
     subject_id: int = Field(foreign_key="subject.id", nullable=False)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
 
-    __table_args__ = (
-        CheckConstraint("question_count>0", name="question_count_non_negative"),
-    )
-
 class SubjectTestCreate(SubjectTestBase):
-    subject_id: int = Field(default=None, foreign_key="subject.id")
-    question_type: str = Field(default=None)
-    question_count: int = Field(default=None)
+    # Potenziell mehr Daten, je nachdem was für Erstellung in n8n gebraucht wird
+    subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
+    question_type: Optional[str] = Field(nullable=False)
 
 class SubjectTestRead(SubjectTestBase):
     id: int
-    test: Optional[str]
+    # Gibt es eine Möglichkeit das direkt als XML abzuspeichern?
+    test: str
     question_type: str
-    question_count: int
     subject_id: int
     created_at: datetime
     updated_at: datetime
-
-class SubjectTestUpdate(SQLModel):
-    name: Annotated[Optional[str], StringConstraints(strip_whitespace=True, min_length=1)] = None
