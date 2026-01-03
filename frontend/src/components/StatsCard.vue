7dcb3122a2fa861a3018e4a6ac338f5bd27047af
Emma Kirchner <emma.kirchner@develappers.de>
2026-01-03 12:08:37 +0100

    add stats card component

---
+++ frontend/src/components/StatsCard.vue
@@ -0,0 +1,101 @@
+﻿<template>
+    <div class="stats-card">
+        <div class="title">{{ title }}</div>
+
+        <div class="main">
+            <div class="icon">
+                <img v-if="iconSrc"
+                     :src="iconSrc"
+                     alt=""
+                     class="icon-img" />
+            </div>
+
+            <div class="value">{{ value }}</div>
+        </div>
+
+        <div v-showS="subtitle" class="subtitle" :class="subtitleClass">
+            {{ subtitle }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "StatsCard",
+
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            value: {
+                type: String,
+                required: true
+            },
+            subtitle: {
+                type: String,
+                default: ""
+            },
+            subtitleClass: {
+                type: String,
+                default: ""
+            },
+            iconSrc: {
+                type: String,
+                required: true
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .stats-card {
+        background: #F3F3F3;
+        border-radius: 22px;
+        padding: 16px 20px;
+        min-width: 300px;
+    }
+
+    .title {
+        color: #6B6B6B;
+        font-size: 16px;
+        margin-bottom: 16px;
+    }
+
+    .main {
+        display: flex;
+        align-items: center;
+        margin-bottom: 3px;
+    }
+
+    .icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 10px;
+    }
+
+    .icon-img {
+        width: 34px;
+        height: 34px;
+    }
+
+    .value {
+        font-size: 28px;
+        font-weight: 500;
+    }
+
+    .subtitle {
+        font-size: 14px;
+        color: #6B6B6B;
+        min-height: 1rem;
+    }
+
+    .subtitle-positive {
+        color: #2E8B57;
+    }
+
+    .subtitle-negative {
+        color: #D90202;
+    }
+</style>
